8abeb44079af4d2b86ace1143c02e7e07c554d81
Jonathan Ehwald <github@ehwald.info>
2026-01-09 20:25:22 +0100

    Upgrade python syntax using pyupgrade

--- keyvalues1/transformers.py
+++ keyvalues1/transformers.py
@@ -1,5 +1,3 @@
-from typing import List
-
 from lark import Token, Transformer
 
 from keyvalues1.types import KV1Dict, KV1Pair, KV1String, KV1Value
@@ -9,11 +7,11 @@ class TreeToJson(Transformer[Token, KV1Pair]):
     def pair(self, items: KV1Pair) -> KV1Pair:
         return items
 
-    def value(self, items: List[KV1Value]) -> KV1Value:
+    def value(self, items: list[KV1Value]) -> KV1Value:
         return items[0]
 
-    def dict(self, items: List[KV1Pair]) -> KV1Dict:
+    def dict(self, items: list[KV1Pair]) -> KV1Dict:
         return dict(items)
 
-    def string(self, items: List[str]) -> KV1String:
+    def string(self, items: list[str]) -> KV1String:
         return items[0].strip('"')
