abe774c1d390ddc906a250c5c702df2bf63857fb
Jonathan Ehwald <github@ehwald.info>
2026-01-16 21:34:46 +0100

    Drop support for Python 3.9

--- keyvalues1/types.py
+++ keyvalues1/types.py
@@ -1,6 +1,4 @@
-from typing import Union
-
-from typing_extensions import TypeAlias
+from typing import TypeAlias, Union
 
 KV1Pair: TypeAlias = tuple["KV1String", "KV1Value"]
 KV1Value: TypeAlias = Union["KV1String", "KV1Dict"]
